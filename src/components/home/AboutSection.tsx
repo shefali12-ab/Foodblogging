8d2174ad75ce1b4f6fd19aca75319bf8a515f772
shefali12-ab <kanaujiashefali@gmail.com>
2024-02-03 20:49:56 +0530

    fixed error

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -15,7 +15,7 @@ const AboutSection = () => {
             <div className='flex flex-col gap-[28px]   w-[80%] lg:w-[70%] text-center lg:text-justify '>
             <h1 className='text-[#0E2368] text-5xl font-semibold font-poppins'>About Us</h1>
             <p className='font-normal text-[#444957] text-sm lg:text-base leading-[27px] font-open-sans'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. t has survived not only five centuries.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos; standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. t has survived not only five centuries.
             </p>
 
             </div>
